8427d7a51913d80f23421cd0722aacd850db4077
carru <carru400@gmail.com>
2014-04-11 00:45:38 +0200

    Remove unused import

--- REMPARK_Rx/src/com/carruesco/pfc/rempark_rx/MasterActivity.java
+++ REMPARK_Rx/src/com/carruesco/pfc/rempark_rx/MasterActivity.java
@@ -14,7 +14,6 @@ import android.os.Bundle;
 import android.support.v4.app.ActionBarDrawerToggle;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
